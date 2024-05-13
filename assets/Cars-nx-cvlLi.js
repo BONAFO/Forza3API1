5192e17b4389e14d626f891a75a40c71bfc00fb5
BONAFO <91232074+BONAFO@users.noreply.github.com>
2024-05-13 12:14:40 -0300

    Add files via upload

---
+++ assets/Cars-nx-cvlLi.js
@@ -0,0 +1,1 @@
+import{N as a,b as e,c,x as s}from"./index-DCpDmlsw.js";const t={};function n(r,o){return e(),c("h3",null,s(r.$route.params.marca),1)}const _=a(t,[["render",n]]);export{_ as default};

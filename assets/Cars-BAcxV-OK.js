126102e7902622cea76c7df29beb2f3eb4f92bc3
BONAFO <91232074+BONAFO@users.noreply.github.com>
2024-05-13 11:16:21 -0300

    Add files via upload

---
+++ assets/Cars-BAcxV-OK.js
@@ -0,0 +1,1 @@
+import{M as a,b as e,c,x as s}from"./index-BX0QUL00.js";const t={};function n(r,o){return e(),c("h3",null,s(r.$route.params.marca),1)}const _=a(t,[["render",n]]);export{_ as default};

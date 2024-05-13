11f656d506603c0d8dd41816485c847942511811
BONAFO <91232074+BONAFO@users.noreply.github.com>
2024-05-13 11:42:40 -0300

    Add files via upload

---
+++ assets/Cars-CtOmEWjG.js
@@ -0,0 +1,1 @@
+import{M as e,b as n,c as o,x as r,L as t}from"./index-7Yo2jNuR.js";const _={__name:"Cars",setup(s){const a=sessionStorage.getItem("marca");return a==null&&(window.location.href=window.origin+e),(c,i)=>(n(),o("h3",null,r(t(a)),1))}};export{_ as default};
